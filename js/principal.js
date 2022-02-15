78ab33b8425196939e27119f63aa71bbc78f2421
Rodrigo Gabriel Gossi <66446783+rowtigo@users.noreply.github.com>
2022-02-15 14:14:11 +0000

    boas praticas

---
+++ js/principal.js
@@ -0,0 +1,8 @@
+//alert("Olá mundo!");
+console.log("Carregado de um arquivo externo!");
+      
+//armazena o elemento H1 na varável titulo
+var titulo = document.querySelector('.titulo');
+
+//Altera o conteúdo da variável titulo
+titulo.textContent = "Nuticionista Aparecida";
