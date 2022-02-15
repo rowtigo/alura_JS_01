b0b1126d9f6a00a651d89a9d799c75abf34a3447
Rodrigo Gabriel Gossi <66446783+rowtigo@users.noreply.github.com>
2022-02-15 18:47:03 +0000

    obtendo dados do paciente

--- js/principal.js
+++ js/principal.js
@@ -6,3 +6,15 @@ var titulo = document.querySelector('.titulo');
 
 //Altera o conteúdo da variável titulo
 titulo.textContent = "Nuticionista Aparecida";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var alturaPaciente = paciente.querySelector(".info-altura");
+var altura = alturaPaciente.textContent;
+
+var pesoPaciente = paciente.querySelector(".info-peso").textContent;
+var peso = pesoPaciente.textContent
+
+var imcPaciente = peso/(altura*altura);
+
+alert(imcPaciente);
