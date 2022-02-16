e8ac4afe379fdd92bcedb9e0d4118313b9f37d37
Rodrigo Gabriel Gossi <66446783+rowtigo@users.noreply.github.com>
2022-02-16 10:32:11 +0000

    Estruturas de repetição

--- js/principal.js
+++ js/principal.js
@@ -7,34 +7,39 @@ var titulo = document.querySelector('.titulo');
 //Altera o conteúdo da variável titulo
 titulo.textContent = "Nuticionista Aparecida";
 
-var paciente = document.querySelector("#primeiro-paciente");
-
-var tdAltua = paciente.querySelector(".info-altura");
-var altura = tdAltua.textContent;
-
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent
-
-var tdImc = paciente.querySelector(".info-imc");
+//Armazena uma lista de pacientes
+var pacientes = document.querySelectorAll(".paciente");
 
 //------------------------------------------------------
-//Validações de altura e peso
 
-var alturaEhValida = true;
-var pesoEhValido = true;
+for(i=0 ; i<pacientes.length ; i++){
 
-if(peso <= 0 || peso >= 1000) {
-  pesoEhValido = false;
-  tdImc.textContent = "Peso é inválido!"
-}
+  //Armazena os elementos da página
+  var tdAltua = pacientes[i].querySelector(".info-altura");
+  var altura = tdAltua.textContent;
+  
+  var tdPeso = pacientes[i].querySelector(".info-peso");
+  var peso = tdPeso.textContent
+  
+  var tdImc = pacientes[i].querySelector(".info-imc");
+  
+  var alturaEhValida = true;
+  var pesoEhValido = true;
 
-if(altura <= 0 || altura >= 3.00) {
-  alturaEhValida = false;
-  tdImc.textContent = "Altura é inválida!"
-}
-
-if(alturaEhValida && pesoEhValido) {
-  var imcPaciente = peso/(altura*altura);
-
-  tdImc.textContent = imcPaciente;
+  //Validações de altura e peso
+  if(peso <= 0 || peso >= 1000) {
+    pesoEhValido = false;
+    tdImc.textContent = "Peso é inválido!"
+  }
+  
+  if(altura <= 0 || altura >= 3.00) {
+    alturaEhValida = false;
+    tdImc.textContent = "Altura é inválida!"
+  }
+  
+  if(alturaEhValida && pesoEhValido) {
+    var imcPaciente = peso/(altura*altura);
+  
+    tdImc.textContent = imcPaciente.toFixed(2);
+  }  
 }
